99727f3059705ddf9e0591b0f458d3692c304653
vincaf <vincenzocafassog@gmail.com>
2022-09-19 18:36:17 +0200

    feat(index): add table w/ values in db posts table

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Index</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Title</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($posts as $post)
+                            <tr>
+                                <th scope="row">{{ $post->id }}</th>
+                                <td>{{ $post->author }}</td>
+                                <td>{{ $post->title }}</td>
+                                <td>
+                                    <a href="" class="btn btn-sm btn-success">
+                                        Edit
+                                    </a>
+                                    <a href="" class="btn btn-sm btn-danger">
+                                        Delete
+                                    </a>
+                                </td>
+                            </tr>
+                        @empty
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection

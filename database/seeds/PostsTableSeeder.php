68ebe164e7cfe5cfa86c49e44c7c9bbb3362083e
vincaf <vincenzocafassog@gmail.com>
2022-09-19 18:23:46 +0200

    feat(PostsTableSeeder): db seeding with faker

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,8 +11,16 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 50; $i++) { 
+            $newPost = new Post();
+            $newPost->title = $faker->realText(35);
+            $newPost->author = $faker->userName();
+            $newPost->post_date = $faker->dateTimeThisYear();
+            $newPost->post_image = $faker->imageUrl();
+            $newPost->post_content = $faker->paragraph(5, true);
+            $newPost->save();
+        }
     }
 }

bdb0a2068c3e565b7b0cc4df47e8050b3653164f
vincaf <vincenzocafassog@gmail.com>
2022-09-20 13:34:31 +0200

    refactor: edit layout post in show.blade

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,16 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-lg">
-        <h2 class="my-3">{{ $post->title }}</h2>
-        <div class="row">
-            <div class="col-4">
-                <img class="img-post" src="{{ $post->post_image }}" alt="{{ $post->title }}">
-            </div>
-            <div class="col-8">
-                <p>{{$post->post_content}}</p>
-                <p><strong>Author:</strong> {{ $post->author }}</p>
-                <p><strong>Post Date:</strong> {{ $post->post_date }}</p>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card my-3" style="max-width: 850px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="{{ $post->post_image }}" alt="{{ $post->post_title }}"
+                            class="img-fluid rounded-start" />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="mb-2"><strong>{{ $post->title }}</strong></h3>
+                            <p><strong>Author:</strong> {{ $post->author }}</p>
+                            <p>{{ $post->post_content }}</p>
+                            <p><strong>Post Date:</strong> {{ $post->post_date }}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>

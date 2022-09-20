237c753dccedd973ecdc8fa45900dfe0bbfedf1b
vincaf <vincenzocafassog@gmail.com>
2022-09-20 12:48:03 +0200

    feat(PostController): add create function

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -27,7 +27,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        $post = new Post();
+        return view('admin.posts.create', compact('post'));
     }
 
     /**

9ca28b531db610c172bcfed89a1ebc587b2a9966
vincaf <vincenzocafassog@gmail.com>
2022-09-20 13:06:49 +0200

    feat(show): add link to post image

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
     <div class="container-lg">
-        <h2 class="my-3">{{$post->title}}</h2>
+        <h2 class="my-3">{{ $post->title }}</h2>
         <div class="row">
             <div class="col-4">
-                <img class="img-fluid" src="https://iltorinese.it/wp-content/themes/fox/images/placeholder.jpg" alt="{{$post->title}}">
+                <img class="img-post" src="{{ $post->post_image }}" alt="{{ $post->title }}">
             </div>
             <div class="col-8">
                 <p>{{$post->post_content}}</p>
-                <p><strong>Author:</strong> {{$post->author}}</p>
-                <p><strong>Post Date:</strong> {{$post->post_date}}</p>
+                <p><strong>Author:</strong> {{ $post->author }}</p>
+                <p><strong>Post Date:</strong> {{ $post->post_date }}</p>
             </div>
         </div>
     </div>

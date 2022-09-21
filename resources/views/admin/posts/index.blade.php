1be0acd1707e70f7c217d1e49cd9980ad2341661
vincaf <vincenzocafassog@gmail.com>
2022-09-21 16:25:43 +0200

    feat(index): add form and script js for delete

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -35,16 +35,41 @@
                                     <a href="{{ route('admin.posts.edit', $post->slug) }}" class="btn btn-sm btn-success">
                                         Edit
                                     </a>
-                                    <a href="" class="btn btn-sm btn-danger">
-                                        Delete
-                                    </a>
+                                    <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST"
+                                        class="form-post-delete" data-post-name="{{ $post->title }}">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button type="submit" class="btn btn-sm btn-danger my-2">
+                                            Delete
+                                        </button>
+                                    </form>
                                 </td>
                             </tr>
                         @empty
+                            <tr>
+                                <td colspan="6">There are no posts available.</td>
+                            </tr>
                         @endforelse
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
+@endsection
+
+@section('footer-scripts')
+    <script>
+        const deleteFormElements = document.querySelectorAll('.form-post-delete');
+        deleteFormElements.forEach(
+            formElement => {
+                formElement.addEventListener('submit', function(event){
+                    const name = this.getAttribute('data-post-name');
+                    event.preventDefault();
+                    const result = window.confirm(`Are you sure you want to delete "${name}"?`);
+                    if(result) this.submit();
+                })
+            }
+        )
+    </script>
 @endsection
